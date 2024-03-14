703990782e42e6aee492301bb627d8c462aada9f
Benjamin Vivanco <benjamin.vivanco@ing.austral.edu.ar>
2024-03-14 05:32:47 -0300

    refactor: binary operation node

--- src/main/kotlin/ast/NodeVisitor.kt
+++ src/main/kotlin/ast/NodeVisitor.kt
@@ -5,7 +5,7 @@ import org.example.ast.nodes.*
 interface NodeVisitor<T> {
     fun visit(declarationNode: DeclarationNode): T
     fun visit(assignationNode: AssignationNode): T
-    fun visit(binaryDeclarationNode: BinaryDeclarationNode): T
+    fun visit(binaryOperationNode: BinaryOperationNode): T
     fun visit(literalNode: LiteralNode): T
     fun visit(identifierNode: IdentifierNode): T
     fun visit(programNode: ProgramNode): T
--- src/main/kotlin/ast/nodes/BinaryOperationNode.kt
+++ src/main/kotlin/ast/nodes/BinaryOperationNode.kt
@@ -2,7 +2,7 @@ package org.example.ast.nodes
 
 import org.example.ast.NodeVisitor
 
-class BinaryDeclarationNode: ExpressionNode() {
+class BinaryOperationNode: ExpressionNode() {
     override fun <T> accept(visitor: NodeVisitor<T>): T {
         return visitor.visit(this)
     }
