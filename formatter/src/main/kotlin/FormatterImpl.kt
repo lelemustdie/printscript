320187141fd1a0eb77dcb9728420913db832f8e3
AlanFierr0 <85326611+AlanFierr0@users.noreply.github.com>
2024-03-28 20:57:55 -0300

    extraction of functions

--- formatter/src/main/kotlin/FormatterImpl.kt
+++ formatter/src/main/kotlin/FormatterImpl.kt
@@ -20,10 +20,19 @@ class FormatterImpl: Formatter{
     private fun evaluate(ast: ProgramNode, rules: FormattingRules): String {
         var string = ""
         for (statement in ast.getStatements()) {
-            when(statement){
-              is StatementNode.PrintNode -> {string += evaluatePrintNode(statement, rules)}
-                is StatementNode.DeclarationNode -> {string += evaluateDeclarationNode(statement, rules)}
-                is StatementNode.AssignationNode -> {string += evaluateAssignationNode(statement, rules)}
+            string += when(statement){
+                is StatementNode.PrintNode -> {
+                    evaluatePrintNode(statement, rules)
+                }
+
+                is StatementNode.DeclarationNode -> {
+                    evaluateDeclarationNode(statement, rules)
+                }
+
+                is StatementNode.AssignationNode -> {
+                    evaluateAssignationNode(statement, rules)
+                }
+
                 else -> throw Exception("Unknown node type")
             }
         }
@@ -32,9 +41,7 @@ class FormatterImpl: Formatter{
 
     private fun evaluatePrintNode(node: StatementNode.PrintNode, rules: FormattingRules): String {
         var string = ""
-        if (rules.numberNewLinesBeforePrint > 0) {
-            string += "\n".repeat(rules.numberNewLinesBeforePrint)
-        }
+        string += newLines(rules.numberNewLinesBeforePrint)
         string += "println("
         string += evaluateExpressionNode(node.printable, rules)
         string += ");"
@@ -43,25 +50,12 @@ class FormatterImpl: Formatter{
     }
 
     private fun evaluateDeclarationNode(node: StatementNode.DeclarationNode, rules: FormattingRules): String {
-        //let:number = 5;
         var string = "let "
         val id = node.variable.identifier.id.value
         string += id
-        if (rules.numberSpacesBeforeColon > 0) {
-            string += " ".repeat(rules.numberSpaceAfterColon)
-        }
-        string += ":"
-        if (rules.numberSpaceAfterColon > 0) {
-            string += " ".repeat(rules.numberSpaceAfterColon)
-        }
+        spacesBetwenOperator(rules.numberSpacesBeforeColon, rules.numberSpaceAfterColon, ":")
         string += type(node.variable.dataType.typeToken.type)
-        if (rules.numberSpaceBeforeAssignation > 0) {
-            string += " ".repeat(rules.numberSpaceBeforeAssignation)
-        }
-        string += "="
-        if (rules.numberSpaceAfterAssignation > 0) {
-            string += " ".repeat(rules.numberSpaceAfterAssignation)
-        }
+        spacesBetwenOperator(rules.numberSpaceBeforeAssignation, rules.numberSpaceAfterAssignation, "=")
         string += evaluateExpressionNode(node.expression, rules)
         string += ";"
         string += "\n"
@@ -71,13 +65,7 @@ class FormatterImpl: Formatter{
     private fun evaluateAssignationNode(node: StatementNode.AssignationNode, rules: FormattingRules): String {
         //nombre = 5;
         var string = node.identifier.id.value
-        if (rules.numberSpaceBeforeAssignation > 0) {
-            string += " ".repeat(rules.numberSpaceBeforeAssignation)
-        }
-        string += "="
-        if (rules.numberSpaceAfterAssignation > 0) {
-            string += " ".repeat(rules.numberSpaceAfterAssignation)
-        }
+        spacesBetwenOperator(rules.numberSpaceBeforeAssignation, rules.numberSpaceAfterAssignation, "=")
         string += evaluateExpressionNode(node.expression, rules)
         string += ";"
         string += "\n"
@@ -119,5 +107,19 @@ class FormatterImpl: Formatter{
             TokenType.TYPE_STRING -> "string"
             else -> throw Exception("Unknown type")
         }
+    }
+
+    private fun spacesBetwenOperator(before: Int, after: Int, operator:String): String {
+        var string = ""
+        string += "".repeat(before)
+        string += operator
+        string += "".repeat(after)
+        return string
+    }
+
+    private fun newLines(before: Int): String {
+        var string = ""
+        string += "\n".repeat(before)
+        return string
     }
 }
